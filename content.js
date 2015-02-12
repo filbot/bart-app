5fd9da3cd6ccffa793ebb8702aa6a76cdd24d56b
Filip Adamczyk <filip@filbot.com>
2015-02-12 09:23:18 -0800

    Convert app to Chrome app

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
-// BART API call
-
+// Schedules call
 $.ajax({
     type: 'GET',
     url: 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=12TH&key=MW9S-E7SL-26DU-VV8V',
@@ -34,3 +33,24 @@ function parseSchedule(xml) {
 
     $('.next-train-time').append('<span>next train ' + times[1] + ' min</span>');
 };
+
+// Advisory call
+$.ajax({
+    type: 'GET',
+    url: 'http://api.bart.gov/api/bsa.aspx?cmd=bsa&key=MW9S-E7SL-26DU-VV8V&date=today',
+    dataType: 'xml',
+    success: parseAdvisory
+});
+
+function parseAdvisory(xml) {
+
+    $(xml).find('description').each(function() {
+        if ($(this).text() === 'No delays reported.') {
+            $('#advisory-list').append('<li>all trains on-time</li>');
+        } else {
+            $('#advisory-list').append('<li>' + $(this).text() + '</li>');
+        };
+    });
+    
+};
+
